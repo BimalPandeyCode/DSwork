83e8cc55b6f0f074a1eaaf77d2d3eb174c57d657
Bimal Pandey <76998974+BimalPandeyCode@users.noreply.github.com>
2022-05-26 21:30:44 +0545

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+struct connect{
+    int weight;
+    int to;
+}
+struct node{
+    int level = 9999;
+    struct connect connection[5];
+}
+int main(){
+    struct node nodes[6] = {
+        
+    }
+    return 0;
+}
